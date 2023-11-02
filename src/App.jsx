5cc51549933bd1aa7478c6473312d9c4471bb90e
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-02 14:54:12 +0330

    Feat: Added Toastify library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Landing, Dashboard, Register, Error } from "./pages"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position="top-center" autoClose={3000}/>
     </BrowserRouter>
   )
 }
